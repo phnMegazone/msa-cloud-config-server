da8636e9f1ae232316a3b282c02ce0b4b1ad4bf3
MZ01-PHN <eu@MZ01-PHN.local>
2022-06-14 14:58:48 +0700

    Initial commit

---
+++ src/main/java/com/lguplus/fleta/MsaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.lguplus.fleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lguplus/fleta/MsaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lguplus.fleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
